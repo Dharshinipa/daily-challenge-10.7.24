f418e428e15c42fc4fb75dd9fa57033dd4285886
Dharshini <126184056+Dharshinipa@users.noreply.github.com>
2024-07-10 06:32:16 +0530

    Update crawler.java

--- crawler.java
+++ crawler.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public int minOperations(String[] logs) {
+        int folderDepth = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (folderDepth > 0) folderDepth--;
+            } 
+            else if (!log.equals("./")) {
+                folderDepth++;
+            }
+        }
+
+        return folderDepth;
+    }
+}
